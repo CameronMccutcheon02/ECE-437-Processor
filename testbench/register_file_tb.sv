07264f8fa460e19fc6bc50184139fefb489fc756
Nicholas James <james222@purdue.edu>
2023-01-15 21:53:13 -0500

    final

--- testbench/register_file_tb.sv
+++ testbench/register_file_tb.sv
@@ -48,5 +48,90 @@ module register_file_tb;
 
 endmodule
 
+task Async_Reset_DUT;
+begin
+  register_file_tb.nRST = 0;
+
+  #(register_file_tb.PERIOD);
+  #(register_file_tb.PERIOD);
+
+  register_file_tb.nRST = 1;
+
+  #(register_file_tb.PERIOD);
+  #(register_file_tb.PERIOD);
+end
+endtask
+
+task Reset_Input;
+begin
+  register_file_tb.rfif.rsel1 = '0;
+  register_file_tb.rfif.rsel2 = '0;
+  register_file_tb.rfif.wsel = '0;
+  register_file_tb.rfif.wdat = '0;
+  register_file_tb.rfif.WEN = 0;
+end
+endtask
+
+task Check_Outputs;
+  input string test_case;
+  input logic [31:0] expected_rdat1;
+  input logic [31:0] expected_rdat2;
+begin
+  if (register_file_tb.rfif.rdat1 == expected_rdat1 && register_file_tb.rfif.rdat2 == expected_rdat2)
+    $display("PASSED: %s", test_case);
+  else $display("FAILED: %s", test_case);
+end
+endtask
+
 program test;
+  integer i;
+  string format;
+  initial begin
+    // ************************************************************************
+    // Test Case 0: Initialize Design
+    // ************************************************************************
+    Reset_Input();
+    Async_Reset_DUT();
+
+    $display("---TEST CASE 0: Initialization---");
+    Check_Outputs("Initialization", '0, '0);
+    $display(""); 
+
+    // ************************************************************************
+    // Test Case 1: Write to Reg0
+    // ************************************************************************
+    Reset_Input();
+    Async_Reset_DUT();
+
+    register_file_tb.rfif.wdat = 32'h12345678;
+    register_file_tb.rfif.WEN = 1;
+    register_file_tb.rfif.wsel = '0;
+    register_file_tb.rfif.rsel1 = '0;
+    register_file_tb.rfif.rsel2 = '0;
+
+    $display("---TEST CASE 1: Writes to Reg 0---");
+    Check_Outputs("Writes to Reg 0", '0, '0);
+    $display("");
+    // ************************************************************************
+    // Test Case 2: Normal Reads and Writes
+    // ************************************************************************
+    Reset_Input();
+    Async_Reset_DUT();
+    
+    $display("---TEST CASE 2: Normal Reads and Writes---");
+    for (i = 1; i < 32; i++) begin
+      #(register_file_tb.PERIOD);
+      register_file_tb.rfif.rsel1 = i;
+      register_file_tb.rfif.rsel2 = i;
+      register_file_tb.rfif.WEN = 1;
+      register_file_tb.rfif.wdat = i;
+      register_file_tb.rfif.wsel = i;
+      #(register_file_tb.PERIOD);
+      $sformat(format,"Reading and Writing to Reg %0d", i);
+      Check_Outputs(format, register_file_tb.rfif.wdat, register_file_tb.rfif.wdat);
+    end
+    $display("");
+
+    $finish;
+  end
 endprogram
