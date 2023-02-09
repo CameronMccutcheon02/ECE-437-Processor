ef67906934fccea88bf6a17c93fc658a5b76dd8e
Cameron McCutcheon <mccutchc@purdue.edu>
2023-02-09 14:08:02 -0500

    bne fix

--- asmFiles/test.bne_nt.asm
+++ asmFiles/test.bne_nt.asm
@@ -3,10 +3,21 @@ org 0x0000
 ori $29, $0, 0xFFFC
 bne $0, $0, BRANCH
 ori $8, $0, 0xABCD
+nop
+nop
+nop
 sw $8, 0x00F0($0)
+nop
+nop
+nop
 halt
 
 BRANCH:
     ori $8, $0, 0x0BAD
+    nop
+    nop
     sw $8, 0x00F0($0)
+    nop
+    nop
+    nop
     halt
--- asmFiles/test.bne_t.asm
+++ asmFiles/test.bne_t.asm
@@ -2,12 +2,29 @@ org 0x0000
 
 ori $29, $0, 0xFFFC
 ori $8, $0, 0x1234
+nop 
+nop 
+nop
 bne $0, $8, BRANCH
+nop 
+nop 
 ori $8, $0, 0x0BAD
+nop 
+nop 
 sw $8, 0x00F0($0)
+nop 
+nop 
+nop
 halt
 
 BRANCH:
+    
     ori $8, $0, 0xABCD
+    nop
+    nop
+    nop
     sw $8, 0x00F0($0)
+    nop
+    nop
+    nop
     halt
