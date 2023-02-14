9dedc4bf2f1cd7f9baf02fda48767b3884f85f5c
Cameron McCutcheon <mccutchc@purdue.edu>
2023-02-13 20:55:43 -0500

    added unittests

--- asmFiles/test.data_hazard.asm
+++ asmFiles/test.data_hazard.asm
@@ -3,7 +3,7 @@ org 0x0000
 ori $29, $0, 0xFFFC
 ori $2, $0, 0x0002
 nop
-nop 
+nop
 nop
 addi $2, $2, 0x2
 addi $2, $2, 0x2
@@ -11,4 +11,30 @@ nop
 nop
 nop
 sw $2, 0x00F0($0)
+
+ori $3, $0, 0x0002
+nop
+nop
+nop
+addi $3, $3, 0x2
+nop
+addi $3, $3, 0x2
+nop
+nop
+nop
+
+sw $3, 0x00F4($0)
+nop
+nop
+nop
+addi $2, $2, 0x2
+addi $3, $3, 0x2
+add $4, $2, $3
+nop
+nop
+nop
+sw $4, 0x00F8($0)
+
+
+
 halt
