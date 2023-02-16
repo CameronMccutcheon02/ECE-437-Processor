33d6d2c9884158e843b6de02cdc67e1ca72b779e
Cameron McCutcheon <mccutchc@purdue.edu>
2023-02-15 22:18:22 -0500

    fixzed unit test

--- asmFiles/test.load_hazard.asm
+++ asmFiles/test.load_hazard.asm
@@ -5,11 +5,11 @@ ori $8, $0, 0xABCD
 
 sw $8, 0x00F0($0) #store to address
 
-ori $8, 0x0BAD
-lw $8, 0x00F0($0) #attempt to load the BAD value
-ori $7, $0, $8
+ori $8, $0, 0x0BAD
+lw $8, 0x00F0($0)
+or $7, $0, $8
  
- 
+
 
 sw $7, 0x00F0($0)
 halt
