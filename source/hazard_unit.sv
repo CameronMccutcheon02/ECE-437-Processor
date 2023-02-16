125929adbd2431690278061c5c1b87cd22df300f
Nicholas James <james222@purdue.edu>
2023-02-16 14:25:54 -0500

    final hazard unit

--- source/hazard_unit.sv
+++ source/hazard_unit.sv
@@ -14,6 +14,8 @@ module hazard_unit (
             if ((huif.Rt_dc == huif.Rs_ft) | (huif.Rt_dc == huif.Rt_ft)) begin
                 huif.flush = 4'b0100; // flush decode/execute latch if load dependency
                 huif.freeze = 4'b1000; // freeze pc and fetch/decode latch if load dependency            end
+            end
+        end
         if (huif.memread_ex) begin   
             if ((huif.Rt_ex == huif.Rs_ft) | (huif.Rt_ex == huif.Rt_ft)) begin // rt in mem == rs or rt in fetch 
                 huif.flush = 4'b0100; // flush decode/execute latch if load dependency
