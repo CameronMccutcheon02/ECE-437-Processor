885fd145756dc4dfb1eddda0351412092a2d01ea
Cameron McCutcheon <mccutchc@purdue.edu>
2023-03-08 18:50:37 -0500

    created icache and dcache files

---
+++ source/icache.sv
@@ -0,0 +1,19 @@
+//Cameron McCutcheon
+
+// interfaces
+`include "datapath_cache_if.vh"
+`include "caches_if.vh"
+
+// cpu types
+`include "cpu_types_pkg.vh"
+import cpu_types_pkg::*;
+
+module icache (
+  datapath_cache_if.cache dcif,
+  caches_if cif
+);
+
+
+
+
+endmodule
