c9cbbf8275d9fd42ed88aa6b6552294415ea1402
Cameron McCutcheon <mccutchc@purdue.edu>
2023-03-28 20:42:10 -0400

    arb logic

--- source/memory_control.sv
+++ source/memory_control.sv
@@ -148,53 +148,15 @@ modport cc
     endcase
   end
 
-// THIS CAN BE DELETED LATER, JUST USED FOR REFERENCE FROM PREV MEM CONTROLLER
+always_comb begin : arbiternextlogic
+    next_mc.arb = mc.arb;
 
-  always_comb begin: MEM_LOGIC
-    // From Cache
-      // iREN
-      // dREN
-      // dWEN
-      // dstore
-      // iaddr
-      // daddr
-    // From RAM
-      // ramload
-      // ramstate
-
-    // the wait signals are inverted to be used as the hit signals by the cache
-    ccif.iwait = 1'b1; // set low when fetching instruction
-    ccif.dwait = 1'b1; // set low when accessing data
-    ccif.iload = '0;
-    ccif.dload = '0;
-
-    ccif.ramREN = 1'b0;
-    ccif.ramWEN = 1'b0;
-    ccif.ramaddr = '0;
-    ccif.ramstore = '0;
-
-    if (ccif.dWEN | ccif.dREN) begin // reading/writing to data memory
-      // TO CACHE
-      ccif.dwait = ~(ccif.ramstate == ACCESS);
-      if (ccif.dREN)
-        ccif.dload = ccif.ramload;
-
-      // TO RAM
-      ccif.ramaddr = ccif.daddr;
-      if (ccif.dWEN) begin
-        ccif.ramWEN = 1'b1;
-        ccif.ramstore = ccif.dstore;
-      end else
-        ccif.ramREN = 1'b1;
-    end else begin // reading from instruction memory
-      // TO CACHE
-      ccif.iwait = ~(ccif.ramstate == ACCESS);
-      ccif.iload = ccif.ramload;
-
-      // TO RAM
-      ccif.ramREN = 1'b1;
-      ccif.ramaddr = ccif.iaddr;
-    end
-  end
+    if (mc_state == IDLE) begin
+      if (ccif.dREN[0] | ccif.dWEN[0] | ccif.iREN[0]) 
+        next_mc.arb = 0;
+      else if (ccif.dREN[1] | ccif.dWEN[1] | ccif.iREN[1])
+        next_mc.arb = 1;
+    end 
+end
 
 endmodule
